3ba532db527159c3d762b59d954ea9da5b484eef
madhava96 <madhavas@brightcomgroup.com>
2023-01-05 03:37:02 -0800

    task5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    # if c is not in c
+    lst=[]
+    for x in my_string:
+        if x!='c' and x != 'C':
+            lst.append(x)
+    return "".join(lst)

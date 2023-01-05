5aa2cf870532c96940e235342ca3a2fb2eba8678
madhava96 <madhavas@brightcomgroup.com>
2023-01-05 03:41:03 -0800

    task5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     # if c is not in c
-    lst= []
+    lst = []
     for x in my_string:
-        if x!= 'c' and x != 'C':
+        if x != 'c' and x != 'C':
             lst.append(x)
     return "".join(lst)

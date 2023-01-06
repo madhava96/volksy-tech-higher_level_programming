b46a9919d8a5980b4aa07d20af85617a6d1c2788
madhava96 <madhavas@brightcomgroup.com>
2023-01-06 02:34:13 -0800

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -7,4 +7,4 @@ def print_matrix_integer(matrix=[[]]):
                     print('{:d}'.format(j), end=" ")
                 else:
                     print(' {:d}'.format(j), end=" ")
-        print()
+            print()

4fbaf10ed0df51c95fe62ad662baf7fbb30c080c
madhava96 <madhavas@brightcomgroup.com>
2023-01-06 02:12:17 -0800

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix):
+    for i in range(len(matrix)):
+        for j in matrix[i]:
+            if j == matrix[i][-1]:
+                print(j)
+            else:
+                print('{:d}'.format(j), end=" ")

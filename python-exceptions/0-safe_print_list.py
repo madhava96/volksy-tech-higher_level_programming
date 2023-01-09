e7cb774ce31fefb72ac7c833bd330c9362289369
madhava96 <madhavas@brightcomgroup.com>
2023-01-09 02:47:42 -0800

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    e=0
-    for i in range(x, 0):
+    e= 0
+    for i in range(0, x):
         try:
             print("{:d}" .format(my_list[a]), end="")
             e +=1

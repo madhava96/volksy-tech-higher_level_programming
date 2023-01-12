a73d325ff9ca54369ce954694cfdab9d32283b91
madhava96 <madhavas@brightcomgroup.com>
2023-01-12 02:43:52 -0800

    task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
+
+
+def add_integer(a, b = 98):
+
     '''integers addition'''
     if (type(a) != int and type(a) != float) or (type(b) != int and type(b) != float):
         raise TypeError("a must be an integer") or TypeError("b must be an integer")

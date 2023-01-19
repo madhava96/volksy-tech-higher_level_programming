6dd61eacf5e5bbe19f2e3e60a114b3b666673d25
madhava96 <madhavas@brightcomgroup.com>
2023-01-18 21:06:08 -0800

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-# 1-number_of_lines.py
-# Brennan D Baraban <375@holbertonschool.com>
+"""
+    function that returns the number
+    of lines of a text file:
 
+    Prototype: def number_of_lines(filename=""):
+    You must use the with statement
+    You don’t need to manage file permission or
+    file doesn't exist exceptions.
+"""
 
-"""Defines a text file line-counting function."""
 
 def number_of_lines(filename=""):
-    """Return the number of lines in a text file."""
-    lines = 0
-    with open(filename) as f:
-        for line in f:
-            lines += 1
-    return lines
+    """ return the number of lines of a file """
+    i = 0
+    with open(filename, encoding='utf-8') as file:
+        for line in file:
+            i += 1
+    return i

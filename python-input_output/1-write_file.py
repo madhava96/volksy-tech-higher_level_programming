202a7797f6a54b4d67f3eb1990059f4c13a2730a
madhava96 <madhavas@brightcomgroup.com>
2023-01-18 21:03:42 -0800

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # 1-number_of_lines.py
 # Brennan D Baraban <375@holbertonschool.com>
-"""Defines a text file line-counting function."""
 
+
+"""Defines a text file line-counting function."""
 
 def number_of_lines(filename=""):
     """Return the number of lines in a text file."""

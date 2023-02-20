4c11ebb1666e6bf2c2a4202c738d19e1440ec752
madhava96 <madhavas@brightcomgroup.com>
2023-02-20 02:28:06 -0800

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,8 +4,8 @@ if (n > 0) {
   let i;
   for (i = 0; i < n; i++) {
     console.log('C is fun')
-	}
+  }
 }
-else if (isNaN(n)){
-	console.log('Missing number of occurrences')
+else if (isNaN(n)) {
+  console.log('Missing number of occurrences')
 }

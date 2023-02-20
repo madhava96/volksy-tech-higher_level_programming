9454fbe4e67aa99632d846506a4d8d579fc15e76
madhava96 <madhavas@brightcomgroup.com>
2023-02-20 02:11:12 -0800

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 n = Number.parseInt(process.argv[2]);
 if (n> 0) {
-  for (i=0; i<n; i++){
-    console.log('C is fun')
-  }
-}
-  else if (isNaN(n)){
-  console.log('Missing number of occurrences')
+	for (i = 0; i< n; i++){
+        console.log('C is fun')
+	}
+else if (isNaN(n)){
+	console.log('Missing number of occurrences')
 }
